a8a08de273d81402a7761cac1fa8f2dc970f55c0
mkrticho <48924549+mkrticho@users.noreply.github.com>
2023-05-29 13:24:30 -0700

    Tested API requests from data.lacity.org

---
+++ api_request_test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sodapy import Socrata
+from config import app_token, username, password
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+# client = Socrata("data.lacity.org", None)
+
+# Example authenticated client (needed for non-public datasets):
+client = Socrata("data.lacity.org",
+                 app_token,
+                 username=username,
+                 password=password)
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("wjz9-h9np", limit=20)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+
+print(results_df.head())
